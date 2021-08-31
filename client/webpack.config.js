692d9196b7924f44d48d37bd6a0835fffa7315b1
cihat <cihat02020202@gmail.com>
2021-08-31 23:39:09 +0300

    Stand up cliend and server

---
+++ client/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              indentedSyntax: true,
+              // sass-loader version >= 8
+              sassOptions: {
+                indentedSyntax: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
